e65c5815492e360e7d24351d07aa14ec36a70347
Nick Wylynko <nick@wylynko.com>
2022-08-24 18:18:53 +0800

    Add React server

--- index.tsx
+++ index.tsx
@@ -1,1 +1,28 @@
-console.log("Hello via Bun!");
+import { renderToReadableStream } from "react-dom/server";
+
+const dt = new Intl.DateTimeFormat();
+
+const port = Number(process.env.PORT ?? "8080");
+
+export default {
+  port,
+  async fetch(request: Request) {
+    const url = new URL(request.url);
+    return new Response(
+      await renderToReadableStream(
+        <html>
+          <head>
+            <title>Hello World</title>
+          </head>
+          <body>
+            <h1>Hello from Bun & React!</h1>
+            <p>The date is {dt.format(new Date())}</p>
+            <span>You requested {url.pathname + url.search}</span>
+          </body>
+        </html>
+      )
+    );
+  },
+};
+
+console.log(`listening on port ${port}`);
