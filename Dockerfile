aadde0fe88d0f92d55e04d05496e74bb113aad04
Nick Wylynko <nick@wylynko.com>
2022-08-25 16:45:21 +0800

    Update dockerfile to bundle up node modules

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,8 @@ COPY . .
 
 RUN bun install
 
+# this bundles up the node modules so they load faster
+# great for our serverless deployment
+RUN bun bun ./index.tsx
+
 CMD ["bun", "run", "./index.tsx"]
--- index.tsx
+++ index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { renderToReadableStream } from "react-dom/server";
 
 const dt = new Intl.DateTimeFormat();
@@ -8,6 +9,8 @@ export default {
   port,
   async fetch(request: Request) {
     const url = new URL(request.url);
+    const { env } = process;
+    console.log({ env });
     return new Response(
       await renderToReadableStream(
         <html>
@@ -18,6 +21,7 @@ export default {
             <h1>Hello from Bun & React!</h1>
             <p>The date is {dt.format(new Date())}</p>
             <span>You requested {url.pathname + url.search}</span>
+            <pre>{JSON.stringify(env.toJSON(), null, 2)}</pre>
           </body>
         </html>
       )
